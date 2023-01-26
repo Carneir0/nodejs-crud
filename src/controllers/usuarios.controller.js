438570c17b453f9b3067fc6b4bf64650e4995755
Carneir0 <anderson.hcar@gmail.com>
2023-01-26 08:11:26 -0300

    feature: Implementando as funções dos controllers e dos services

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -29,12 +29,47 @@ module.exports = {
     }
   },
   async retornaUsuario(req, res) {
-    res.status(201).send(`Retorna um usuário`);
+    try {
+      // Busca todos os usuários
+      const response = await userService.getOne(req.params.id);
+
+      // Retorna o resultado da operação
+      return res.status(response.status).send(response.mensagem);
+    } catch (error) {
+      console.log(`Erro ao retornar usuário: ${error.message}`);
+      return res.status(500).send(`Erro ao retornar usuário!`);
+    }
   },
   async atualizaUsuario(req, res) {
-    res.status(201).send(`Atualiza um usuário`);
+    try {
+      const { nome, cpf, email, telefone } = req.body;
+
+      // Busca todos os usuários
+      const response = await userService.update(
+        req.params.id,
+        nome,
+        cpf,
+        email,
+        telefone
+      );
+
+      // Retorna o resultado da operação
+      return res.status(response.status).send(response.mensagem);
+    } catch (error) {
+      console.log(`Erro ao atualizar usuário: ${error.message}`);
+      return res.status(500).send(`Erro ao atualizar usuário!`);
+    }
   },
   async deletaUsuario(req, res) {
-    res.status(201).send(`Deleta um usuário`);
+    try {
+      // Busca todos os usuários
+      const response = await userService.delete(req.params.id);
+
+      // Retorna o resultado da operação
+      return res.status(response.status).send(response.mensagem);
+    } catch (error) {
+      console.log(`Erro ao remover usuário: ${error.message}`);
+      return res.status(500).send(`Erro ao remover usuário!`);
+    }
   },
 };
--- src/services/usuarios.service.js
+++ src/services/usuarios.service.js
@@ -1,51 +1,52 @@
 const dateTime = require("node-datetime");
 
-const User = require("../models/usuarios.model");
+const Usuario = require("../models/usuarios.model");
 
 module.exports = {
   async create(nome, cpf, email, telefone) {
     // Verificação de duplicidade pelo email
-    const user = await User.findOne({
+    const usuarioExistente = await Usuario.findOne({
       where: {
         email: email,
       },
       raw: true,
     });
 
-    if (user) {
+    if (usuarioExistente) {
       return {
         status: 400,
         mensagem: "Email já utilizado",
       };
-    } else {
-      const created_at = dateTime.create().format("Y-m-d H:M:S");
-      const updated_at = dateTime.create().format("Y-m-d H:M:S");
-
-      // Registra o usuário na base de dados
-      const user = await User.create({
-        nome,
-        cpf,
-        email,
-        telefone,
-        created_at,
-        updated_at,
-      });
-
-      if (user) {
-        return {
-          status: 201,
-          mensagem: user,
-        };
-      } else {
-        return {
-          status: 500,
-          mensagem: "Erro ao criar novo usuário",
-        };
-      }
     }
+
+    // TODO: melhorar essa criação de data aqui
+    const created_at = dateTime.create().format("Y-m-d H:M:S");
+    const updated_at = dateTime.create().format("Y-m-d H:M:S");
+
+    // Registra o usuário na base de dados
+    const usuarioNovo = await Usuario.create({
+      nome,
+      cpf,
+      email,
+      telefone,
+      created_at,
+      updated_at,
+    });
+
+    if (!usuarioNovo) {
+      return {
+        status: 500,
+        mensagem: "Erro ao criar novo usuário",
+      };
+    }
+
+    return {
+      status: 201,
+      mensagem: usuarioNovo,
+    };
   },
   async getAll() {
-    const usuarios = await User.findAll({
+    const usuarios = await Usuario.findAll({
       raw: true,
     });
 
@@ -60,5 +61,120 @@ module.exports = {
         mensagem: "Nenhum usuário encontrado",
       };
     }
+  },
+  async getOne(usuarioId) {
+    const usuario = await Usuario.findOne({
+      where: {
+        id: usuarioId,
+      },
+    });
+
+    if (!usuario) {
+      return {
+        status: 400,
+        mensagem: `Usuário ${usuarioId} não encontrado!`,
+      };
+    }
+
+    return {
+      status: 200,
+      mensagem: usuario,
+    };
+  },
+  async update(usuarioId, nome, cpf, email, telefone) {
+    const usuarioExistente = Usuario.findOne({
+      where: {
+        id: usuarioId,
+      },
+    });
+
+    if (!usuarioExistente) {
+      return {
+        status: 400,
+        mensagem: `Usuário ${usuarioId} não encontrado!`,
+      };
+    }
+
+    if (nome) usuarioExistente["nome"] = nome;
+    if (cpf) usuarioExistente["cpf"] = cpf;
+    if (telefone) usuarioExistente["telefone"] = telefone;
+
+    if (email) {
+      const emailExistente = await Usuario.findOne({
+        where: {
+          email: email,
+        },
+        raw: true,
+      });
+
+      if (emailExistente) {
+        return {
+          status: 400,
+          mensagem: `Email já utilizado!`,
+        };
+      }
+
+      usuarioExistente["email"] = email;
+    }
+
+    const usuarioAtualizado = await Usuario.update(
+      {
+        nome,
+        cpf,
+        email,
+        telefone,
+      },
+      {
+        where: {
+          id: usuarioId,
+        },
+        raw: true,
+      }
+    );
+
+    if (!usuarioAtualizado) {
+      return {
+        status: 500,
+        mensagem: `Erro ao atualizar usuário ${usuarioId}!`,
+      };
+    }
+
+    return {
+      status: 200,
+      mensagem: `Usuário atualizado com sucesso!`,
+    };
+  },
+  async delete(usuarioId) {
+    const usuarioExistente = await Usuario.findOne({
+      where: {
+        id: usuarioId,
+      },
+      raw: true,
+    });
+
+    if (!usuarioExistente) {
+      return {
+        status: 400,
+        mensagem: `Usuário ${usuarioId} não encontrado!`,
+      };
+    }
+
+    const usuarioDeletado = await Usuario.destroy({
+      where: {
+        id: usuarioId,
+      },
+    });
+
+    if (!usuarioDeletado) {
+      return {
+        status: 400,
+        mensagem: `Erro ao remover usuário ${usuarioId}!`,
+      };
+    }
+
+    return {
+      status: 200,
+      mensagem: `Usuário ${usuarioId} removido com sucesso!`,
+    };
   },
 };
